df02e9159912a9e32e5b578e66edeb3a2eecc8f4
Benjamin-Yves Trapp <benji.trapp@gmail.com>
2022-03-05 09:48:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='AWSLoginGuard',
       version='0.0.1',
       description='Keep an eye on the AWS login activities',
-      url='https://github.axa.com/marvin-kraus/cb-auto-patcher',
+      url='https://github.com/BenjiTrapp/AWS-LoginGuard',
       author='Der Benji',
       author_email='nyctophobia@protonmail.com',
       license='MIT Licence',
